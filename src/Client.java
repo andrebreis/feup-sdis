d11e7bd12b4001cfcbfd5ecde0c6784dcdda341b
inesferreira7 <inesferreira789@gmail.com>
2017-03-28 18:35:24 +0100

    Finish lab4 - RMI

--- src/Client.java
+++ src/Client.java
@@ -1,34 +1,35 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
-/**
- * Created by chrx on 2/24/17.
- */
 public class Client {
 
     public static void main(String[] args) throws IOException {
 
         if (args.length < 4) {
-            System.out.println("java Client <host_name> <port_number> <oper> <opnd> * ");
+            System.out.println("java Client <host_name> <remote_object_name> <oper> <opnd> * ");
             return;
         }
 
+        try {
+            Registry registry = LocateRegistry.getRegistry(args[0]);
+            RemoteInterface stub = (RemoteInterface) registry.lookup(args[1]);
 
-        Socket socket = new Socket(args[0], Integer.parseInt(args[1]));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
+            String response = "";
 
-        // send request
-        String cmd = "";
-        for(int i = 2; i < args.length; i++)
-            cmd += args[i] + " ";
-
-        out.println(cmd);
-        System.out.println(in.readLine());
+            if(args[2].equals("register")){
+                if(args.length < 5) return;
+                response = stub.register(args[3],args[4]);
+            }
+            else if (args[2].equals("lookup")){
+                if(args.length < 4) return;
+                response = stub.lookup(args[3]);
+            }
+            System.out.println("response: " + response);
+        } catch (Exception e) {
+            System.err.println("Client exception: " + e.toString());
+            e.printStackTrace();
+        }
 
     }
 
--- src/Server.java
+++ src/Server.java
@@ -1,17 +1,58 @@
-import java.io.IOException;
-import java.net.SocketException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.HashMap;
+import java.util.regex.Pattern;
 
-/**
- * Created by chrx on 2/24/17.
- */
-public class Server {
+public class Server implements RemoteInterface {
+    private HashMap<String,String> licensePlates;
 
-    public static void main(String[] args) throws IOException {
-        if(args.length != 1){
-            System.out.println("java Server <srvc_port>");
-            return;
-        }
-        new ServerThread(Integer.parseInt(args[0])).start();
+
+    public Server() {
+        licensePlates = new HashMap<>();
     }
 
+    public static void main(String args[]) {
+
+        if (args.length < 1) {
+            System.out.println("java Server <remote_object_name>");
+            return;
+        }
+
+        try {
+            Server obj = new Server();
+            RemoteInterface stub = (RemoteInterface) UnicastRemoteObject.exportObject(obj, 1099);
+
+            LocateRegistry.createRegistry(1099);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind(args[0], stub);
+
+            System.err.println("Server ready");
+        } catch (Exception e) {
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String register(String plate, String owner) throws RemoteException {
+
+        if(!Pattern.matches("\\w\\w-\\w\\w-\\w\\w", plate))
+            return "-1";
+        if(licensePlates.containsKey(plate))
+            return "-1";
+
+        licensePlates.put(plate,owner);
+        return Integer.toString(licensePlates.size());
+
+    }
+
+    @Override
+    public String lookup(String plate) throws RemoteException {
+
+        if(!licensePlates.containsKey(plate))
+            return "NOT_FOUND";
+        return licensePlates.get(plate);
+    }
 }
