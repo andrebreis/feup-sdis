32623f2c5ad5793cb5bd61fb9321b2c1e359124d
Andre <andrebrreis@gmail.com>
2017-04-06 15:20:40 +0100

    Improve chunk backup/restore functionalities

--- src/file_manager/FileManager.java
+++ src/file_manager/FileManager.java
@@ -11,35 +11,20 @@ import java.util.Scanner;
  */
 public class FileManager {
 
+    final private static String chunksDirectory = "chunks/";
+    final private static String restoredFilesDirectory = "restored_files/";
+
     final private static int MAX_CHUNK_SIZE = 64 * 1000;
 
     //http://stackoverflow.com/questions/10864317/how-to-break-a-file-into-pieces-using-java
 
     public static void splitFile(File f) throws IOException {
-        int partCounter = 1;//I like to name parts from 001, 002, 003, ...
-        //you can change it to 0 if you want 000, 001, ...
 
-        byte[] buffer = new byte[MAX_CHUNK_SIZE];
-
-        try (BufferedInputStream bis = new BufferedInputStream(
-                new FileInputStream(f))) {//try-with-resources to ensure closing stream
-            String name = f.getName();
-
-            int tmp = 0;
-            do {
-                tmp = bis.read(buffer);
-                        //write each chunk of data into separate file with different number in name
-                File newFile = new File(f.getParent(), name + "."
-                        + String.format("%05d", partCounter++));
-                try (FileOutputStream out = new FileOutputStream(newFile)) {
-                    out.write(buffer, 0, tmp == -1 ? 0 : tmp);//tmp is chunk size
-                }
-            } while (tmp == MAX_CHUNK_SIZE);
-        }
     }
 
     public static void mergeFiles(ArrayList<File> files, File into)
             throws IOException {
+        into.getParentFile().mkdirs();
         try (BufferedOutputStream mergingStream = new BufferedOutputStream(
                 new FileOutputStream(into))) {
             for (File f : files) {
@@ -58,17 +43,54 @@ public class FileManager {
         return chunkList;
     }
 
+    public static String getFileId(File file){
+        return "";
+    }
+
+    public static void backupFile(String filepath, int replicationDegree){
+        int partCounter = 1;//I like to name parts from 001, 002, 003, ...
+        //you can change it to 0 if you want 000, 001, ...
+
+        File file = new File(filepath);
+
+        byte[] buffer = new byte[MAX_CHUNK_SIZE];
+
+        try (BufferedInputStream bis = new BufferedInputStream(
+                new FileInputStream(file))) {//try-with-resources to ensure closing stream
+            String name = file.getName();
+
+            int tmp = 0;
+            do {
+                tmp = bis.read(buffer);
+                //write each chunk of data into separate file with different number in name
+                File newFile = new File(chunksDirectory, name + "."
+                        + String.format("%05d", partCounter++));
+                newFile.getParentFile().mkdirs();
+                    backupChunk(buffer, tmp == -1 ? 0 : tmp, replicationDegree, getFileId(file), Integer.toString(partCounter));
+            } while (tmp == MAX_CHUNK_SIZE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void backupChunk(byte[] chunk, int chunkSize, int replicationDegree, String fileId, String chunkNo){
+        //TODO: SEND CHUNK TO MBC
+        System.out.println(new String(chunk));
+    };
+
     public static void main(String[] args) throws IOException {
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         System.out.println("Enter the full path of the file you want to split");
         String path = reader.nextLine();
-        splitFile(new File(path));
-        File thisFolder = new File(".");
-        ArrayList<File> chunks = getChunks(path, thisFolder);
-        for (File f:
-             chunks) {
-            System.out.println(f.getName());
-        }
-        mergeFiles(chunks, new File("new" + path));
+        backupFile(path,1);
+//        File toSplit = new File(path);
+//        splitFile(toSplit);
+//        File chunksContainer = new File(chunksDirectory);
+//        ArrayList<File> chunks = getChunks(toSplit.getName(), chunksContainer);
+//        for (File f:
+//             chunks) {
+//            System.out.println(f.getName());
+//        }
+//        mergeFiles(chunks, new File(restoredFilesDirectory + toSplit.getName()));
     }
 }
