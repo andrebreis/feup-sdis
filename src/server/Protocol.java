afedb02abb7bfdf13d0ce6cd7140ecce5128f349
inesferreira7 <inesferreira789@gmail.com>
2017-04-06 17:51:32 +0100

    Function calls from interface

--- src/server/Protocol.java
+++ src/server/Protocol.java
@@ -1,7 +1,19 @@
 package server;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
 /**
  * Created by ines on 29-03-2017.
  */
-public class Protocol {
+public interface Protocol extends Remote{
+
+    void backup(String version, String senderId, String fileId, int chunkNo, int replicationDegree) throws RemoteException;
+
+    void restore(String version, String senderId, String fileId, int chunkNo) throws RemoteException;
+
+    void delete(String version, String senderId, String fileId) throws RemoteException;
+
+    void reclaim(String version, String senderId, String fileId, int chunkNo) throws RemoteException;
+
 }
